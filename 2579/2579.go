818cb318b26d930263cbf9801828f55caed5ce9e
chjcmy <chjcmy@naver.com>
2021-12-12 18:48:43 +0900

    sol 2579

--- 2579/2579.go
+++ 2579/2579.go
@@ -6,45 +6,69 @@ import (
 	"os"
 )
 
-var reader *bufio.Reader = bufio.NewReader(os.Stdin)
-var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
-
-var (
-	dp    [][]int
-	count int
-)
-
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
 
-	fmt.Fscanln(reader, &count)
-	dp = make([][]int, count+1)
-	dp[0] = append(dp[0], 0)
-	for i := 1; i < count+1; i++ {
-		var num int
-		fmt.Fscanln(reader, &num)
-		dp[i] = append(dp[i], num)
-	}
-	for i := 0; i < count-3; {
-		z := Max(i)
-		dp[i+z][0] = dp[i][0] + dp[i+z][0]
-		i += z
+	var N int
+	fmt.Fscanln(reader, &N)
 
+	arr, dp := make([]int, N+1), make([]int, N+1)
+	for i := 1; i <= N; i++ {
+		fmt.Fscanln(reader, &arr[i])
 	}
-	fmt.Fprintln(writer, Maxing(dp[count-1][0], dp[count-2][0]))
+	dp[1] = arr[1]
+	if N >= 2 {
+		dp[2] = arr[1] + arr[2]
+	}
+	for i := 3; i <= N; i++ {
+		dp[i] = max(dp[i-2], dp[i-3]+arr[i-1]) + arr[i]
+	}
+	fmt.Fprint(writer, dp[N])
 	writer.Flush()
 }
 
-func Max(a int) int {
-	x := dp[a][0] + dp[a+1][0] + dp[a+3][0]
-	y := dp[a][0] + dp[a+2][0] + dp[a+3][0]
-	if x > y {
-		return 1
+func max(first, second int) int {
+	if first >= second {
+		return first
 	}
-	return 2
+	return second
 }
-func Maxing(x, y int) int {
-	if x > y {
-		return dp[count][0] + x
-	}
-	return dp[count][0] + y
-}
+
+//package main
+//
+//import (
+//	"bufio"
+//	"fmt"
+//	"os"
+//)
+//
+//var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+//var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+//
+//func main() {
+//	var dp []int
+//	var count int
+//	fmt.Fscanln(reader, &count)
+//	stairs := make([]int, 300)
+//	for i := 0; i < count; i++ {
+//		var num int
+//		fmt.Fscanln(reader, &num)
+//		stairs[i] = num
+//	}
+//	dp = append(dp, stairs[0])
+//	dp = append(dp, Max(stairs[0] + stairs[1], stairs[0]))
+//	dp = append(dp, Max(stairs[0]+stairs[2], stairs[1]+stairs[2]))
+//	for i := 3; i < count; i++{
+//			dp = append(dp, Max(dp[i-2]+stairs[i], stairs[i-1]+stairs[i]+dp[i-3]))
+//	}
+//	fmt.Fprintln(writer, dp[len(dp)-1])
+//	writer.Flush()
+//}
+//
+//func Max(x, y int) int {
+//	if x > y {
+//		return x
+//	}
+//	return y
+//}
