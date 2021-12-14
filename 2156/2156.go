695ff47cfc892424d3041c89cf21cd747ce2a962
chjcmy <chjcmy@naver.com>
2021-12-15 02:52:36 +0900

    sol 2156

--- 2156/2156.go
+++ 2156/2156.go
@@ -19,34 +19,36 @@ func main() {
 
 	fmt.Fscanln(reader, &N)
 
-	wines = make([]int, N)
-	dp = make([]int, N)
-	for i := 0; i < N; i++ {
+	wines = make([]int, N+1)
+	dp = make([]int, N+1)
+	for i := 1; i < N+1; i++ {
 		fmt.Fscanln(reader, &wines[i])
 	}
 	if N == 1 {
-		fmt.Fprintln(writer, wines[0])
+		fmt.Fprintln(writer, wines[1])
 		writer.Flush()
 		return
 	} else if N == 2 {
-		fmt.Fprintln(writer, wines[0]+wines[1])
+		fmt.Fprintln(writer, wines[1]+wines[2])
 		writer.Flush()
 		return
 	}
-	dp[0] = wines[0]
-	dp[1] = dp[0] + wines[1]
-	dp[2] = Max()
+	dp[1] = wines[1]
+	dp[2] = wines[1] + wines[2]
 
-	for i := 3; i < N-1; i++ {
-		dp[i] = Max(dp[i-2]+wines[i], wines[i-1]+wines[i])
+	for i := 3; i <= N; i++ {
+		dp[i] = Max(dp[i-3]+wines[i-1]+wines[i], dp[i-1], dp[i-2]+wines[i])
 	}
-	fmt.Fprintln(writer, dp[N-2]+wines[N-1])
+	fmt.Fprintln(writer, dp[N])
 	writer.Flush()
 }
 
-func Max(a, b int) int {
-	if a > b {
-		return a
+func Max(arr ...int) int {
+	var min int
+	for _, i := range arr {
+		if min < i {
+			min = i
+		}
 	}
-	return b
+	return min
 }
