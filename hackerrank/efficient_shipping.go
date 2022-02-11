f74066338b794abe374724968cb1037a411ed1a3
chjcmy <chjcmy@naver.com>
2022-02-12 01:12:51 +0900

    sol moeeygosa
    Took 1 hour 18 minutes

--- hackerrank/efficient_shipping.go
+++ hackerrank/efficient_shipping.go
@@ -2,33 +2,22 @@ package main
 
 import "fmt"
 
-func remove(slice []int64, s int) []int64 {
-	return append(slice[:s], slice[s+1:]...)
-}
-
 func main() {
 	boxes := []int64{3, 1, 6}
 	unitsPerBox := []int64{2, 7, 4}
 	truckSize := int64(6)
-	fmt.Println(getMaxUnits(boxes, unitsPerBox, truckSize))
+	truefalse := make([]bool, len(boxes))
+	fmt.Println(getMaxUnits(boxes, unitsPerBox, truckSize, truefalse))
 
 }
 
-func getMaxUnits(boxes []int64, unitsPerBox []int64, truckSize int64) (value int64) {
+func getMaxUnits(boxes []int64, unitsPerBox []int64, truckSize int64, bol []bool) (value int64) {
 	// Write your code here
 
-	for truckSize != 0 {
-		bs, bc := bigarr(unitsPerBox)
-		if truckSize > boxes[bc] {
-			truckSize -= boxes[bc]
-			value += boxes[bc] * int64(bs)
-			boxes = remove(boxes, bc)
-			unitsPerBox = remove(unitsPerBox, bc)
-		} else {
-			value += truckSize * int64(bs)
-			truckSize = 0
-		}
+	for i := 0; i < len(boxes); i++ {
+
 	}
+
 	return
 }
 
--- programmer/moeeygosa_test.go
+++ programmer/moeeygosa_test.go
@@ -1,6 +1,8 @@
 package programmer
 
-import "testing"
+import (
+	"testing"
+)
 
 var testval = [][]int{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}}
 
@@ -16,16 +18,10 @@ func sol(pro []int) (result []int) {
 	maxint := 0
 
 	for i := 0; i < len(person); i++ {
-		x := 0
-		for x < len(pro) {
-			for j := 0; j < len(testval[i]); j++ {
-				if pro[x+j] == testval[i][j] {
-					person[i]++
-				}
-				if x+len(testval[i]) > len(pro) {
-					break
-				}
-				x += len(testval[i])
+
+		for j := 0; j < len(pro); j++ {
+			if pro[j] == testval[i][j%len(testval[i])] {
+				person[i]++
 			}
 		}
 
@@ -42,9 +38,9 @@ func maxmin(arr []int, maxint int) (result []int) {
 
 	for i, i2 := range arr {
 		if i2 == maxint {
-			result = append(result, i)
+			result = append(result, i+1)
 		}
 	}
 
-	return result
+	return
 }
